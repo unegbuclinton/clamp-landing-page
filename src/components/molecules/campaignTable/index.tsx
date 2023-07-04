938184c0f8bfadbeb0695f9e127dba93481b9a28
Clinton <unegbuclinton5@gmail.com>
2023-07-04 13:11:15 +0100

    updated campiagn interface

--- src/components/molecules/campaignTable/index.tsx
+++ src/components/molecules/campaignTable/index.tsx
@@ -11,28 +11,28 @@ const CampaignTable = () => {
         <div className=" p-4">STATUS</div>
         <div className=" p-4"></div>
       </div>
-      {campaigns?.map(
-        ({ title, opt, value, customers, points, status }, index) => (
-          <div
-            key={index}
-            className="flex justify-between bg-white border-b border-light-grey"
-          >
-            <div className=" p-4 w-fit">{title}</div>
-            <div className=" p-4">
-              <p>{opt}</p>
-              <p className="text-sm text-battle-grey/60">{`${customers}% of customers`}</p>
-            </div>
-            <div className=" p-4">
-              <p> {points}</p>
-              <p className="text-sm text-battle-grey/60">{value}</p>
-            </div>
-            <div className=" p-4">{status ? "Active" : "PAUSED"}</div>
-            <div className=" p-4">
-              <AiOutlineMore />
-            </div>
+      {campaigns?.map(({ id, name, rules, status }) => (
+        <div
+          key={id}
+          className="flex justify-between bg-white border-b border-light-grey"
+        >
+          <div className="p-4 w-fit">{name}</div>
+          <div className=" p-4">
+            <p>{rules[0].asset.pointValue}</p>
+            <p className="text-sm text-battle-grey/60">{`4% of customers`}</p>
           </div>
-        )
-      )}
+          <div className=" p-4">
+            <p> {rules[0].asset.pointValue}</p>
+            <p className="text-sm text-battle-grey/60">
+              {rules[0].asset.monetaryValue}
+            </p>
+          </div>
+          <div className="p-4">{status ? "Active" : "PAUSED"}</div>
+          <div className=" p-4">
+            <AiOutlineMore className="cursor-pointer" />
+          </div>
+        </div>
+      ))}
       <div className="flex items-center w-full justify-center py-3">
         <p className="text-center font-semibold text-base">See all campaigns</p>
         <AiOutlineRight size={10} />
--- src/utilities/data/activitiesJson.tsx
+++ src/utilities/data/activitiesJson.tsx
@@ -1,25 +1,36 @@
-export const activities: Array<{ title: string; time: string; badge: string }> =
-  [
-    { title: "John Doe earns a point", time: "2 minutes", badge: "tier-one" },
-    {
-      title: "Adam Smith opted into “1 points for every 1N spent”",
-      time: "2 minutes",
-      badge: "tier-two",
-    },
-    {
-      title: "Laura Davids reached tier gold",
-      time: "2 minutes",
-      badge: "tier-one",
-    },
-    {
-      title: "Laura Davids reached tier gold",
-      time: "2 minutes",
-      badge: "tier-two",
-    },
-    { title: "John Doe earns a point", time: "2 minutes", badge: "tier-one" },
-    {
-      title: "Laura Davids reached tier goldt",
-      time: "2 minutes",
-      badge: "tier-two",
-    },
-  ];
+export const activities: Array<{
+  title: string;
+  timestamp: string;
+  badge: string;
+}> = [
+  {
+    title: "John Doe earns a point",
+    timestamp: "2 minutes",
+    badge: "tier-one",
+  },
+  {
+    title: "Adam Smith opted into “1 points for every 1N spent”",
+    timestamp: "2 minutes",
+    badge: "tier-two",
+  },
+  {
+    title: "Laura Davids reached tier gold",
+    timestamp: "2 minutes",
+    badge: "tier-one",
+  },
+  {
+    title: "Laura Davids reached tier gold",
+    timestamp: "2 minutes",
+    badge: "tier-two",
+  },
+  {
+    title: "John Doe earns a point",
+    timestamp: "2 minutes",
+    badge: "tier-one",
+  },
+  {
+    title: "Laura Davids reached tier goldt",
+    timestamp: "2 minutes",
+    badge: "tier-two",
+  },
+];
