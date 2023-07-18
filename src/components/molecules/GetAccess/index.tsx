893f72cd712f158a2bfa2aa401dd38a6bf737323
Clinton <unegbuclinton5@gmail.com>
2023-07-18 08:58:15 +0100

    resolved conflict

--- src/components/molecules/GetAccess/index.tsx
+++ src/components/molecules/GetAccess/index.tsx
@@ -1,58 +1,55 @@
-import ButtonComponent from "@/components/atoms/button";
-import { Form, Input } from "antd";
-import React from "react";
+import ButtonComponent from '@/components/atoms/button'
+import { Form, Input } from 'antd'
+import React from 'react'
 
 const GetAccessModalComponent = () => {
   const onFinish = (values: any) => {
-    console.log(values);
-  };
+    console.log(values)
+  }
   return (
-    <Form onFinish={onFinish} requiredMark="optional">
+    <Form onFinish={onFinish} requiredMark='optional'>
       <Form.Item
-        name="name"
-        label="Your Name"
+        name='name'
+        label='Your Name'
         labelCol={{ span: 24 }}
         wrapperCol={{ span: 24 }}
-        rules={[{ required: true, message: "Please input your name!" }]}
+        rules={[{ required: true, message: 'Please input your name!' }]}
       >
         <Input />
       </Form.Item>
 
       <Form.Item
-        name="email"
-        label="Your Email"
+        name='email'
+        label='Your Email'
         labelCol={{ span: 24 }}
         wrapperCol={{ span: 24 }}
-        rules={[{ required: true, message: "Please input your email!" }]}
+        rules={[{ required: true, message: 'Please input your email!' }]}
       >
         <Input />
       </Form.Item>
       <Form.Item
-<<<<<<< HEAD
-        name="role"
-        label="Your Company role"
+        name='role'
+        label='Your Company role'
         labelCol={{ span: 24 }}
         wrapperCol={{ span: 24 }}
         rules={[
-          { required: true, message: "Please input your role in your company" },
+          { required: true, message: 'Please input your role in your company' },
         ]}
       >
         <Input />
       </Form.Item>
       <Form.Item
-=======
->>>>>>> 3d87a822626014e491d180f94efe8af2db8e0e02
-        name="phone"
-        label="Phone number"
+        name='phone'
+        label='Phone number'
         labelCol={{ span: 24 }}
         wrapperCol={{ span: 24 }}
-        rules={[{ required: true, message: "Please input your phone!" }]}
+        rules={[{ required: true, message: 'Please input your phone!' }]}
       >
         <Input />
       </Form.Item>
-      <ButtonComponent text="Get Access" type="submit" />
+      <ButtonComponent text='Get Access' type='submit' />
     </Form>
-  );
-};
+  )
+}
 
-export default GetAccessModalComponent;
+export default GetAccessModalComponent
