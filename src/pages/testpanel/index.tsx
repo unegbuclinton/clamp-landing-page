deb732fe2a568d3dc660b64bef84da71fe90229f
Solomon Omojola <omojolasolomon@gmail.com>
2023-09-01 02:18:21 +0200

    test panel and gamified campaign entry point

--- src/pages/testpanel/index.tsx
+++ src/pages/testpanel/index.tsx
@@ -10,6 +10,12 @@ const TestPanel = () => {
     <AuthLayout>
       <div className="flex flex-col justify-center pt-12 w-full">
         <h2 className="text-2xl font-bold w-full">Test Panel</h2>
+        <br />
+        <h3 className="text-xl font-bold w-full text-gray-700  mb-8">Gamified Campaign</h3>
+        <div className="w-40">
+          <ButtonComponent type="button" text="Start" />
+        </div>
+        <Form></Form>
       </div>
     </AuthLayout>
   )
