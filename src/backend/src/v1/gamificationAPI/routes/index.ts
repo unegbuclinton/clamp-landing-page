784371937a968d515d1ea9add01afcf60d95d992
Solomon Omojola <omojolasolomon@gmail.com>
2023-09-04 21:20:49 +0200

    fix router mounting

--- src/backend/src/v1/gamificationAPI/routes/index.ts
+++ src/backend/src/v1/gamificationAPI/routes/index.ts
@@ -1,16 +1,14 @@
 import express, { Request, Response } from 'express'
 import gameRouter from './game'
-import ruleRouter from './leaderboard'
-import assetRouter from './game'
+import leaderboardRouter from './leaderboard'
 
 const router = express.Router()
 
 // Mount the routers
 router.use('/games', gameRouter)
-router.use('/rules', ruleRouter)
-router.use('/assets', assetRouter)
+router.use('/leaderboard', leaderboardRouter)
 router.get('/health', (_req: Request, res: Response) => {
-  res.json({ message: 'Clamp API [Core]. Status OK' })
+  res.json({ message: 'Clamp API [Gamification]. Status OK' })
 })
 
 export default router
