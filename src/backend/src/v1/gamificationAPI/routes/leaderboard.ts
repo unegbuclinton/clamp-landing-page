716158e7dfaa8287dfc69d98f35073d6268f90b6
Solomon Omojola <omojolasolomon@gmail.com>
2023-09-05 06:01:44 +0200

    wip api spec

--- src/backend/src/v1/gamificationAPI/routes/leaderboard.ts
+++ src/backend/src/v1/gamificationAPI/routes/leaderboard.ts
@@ -1,10 +1,9 @@
 import express from 'express'
-import { getTopParticipants, updateRankings, getLeaderboard } from '../controllers/leaderboard'
+import { getTopParticipants, getLeaderboard } from '../controllers/leaderboard'
 
 const router = express.Router()
 
 router.get('/:roundId/top/:n', getTopParticipants)
-router.put('/rank/:roundId', updateRankings)
 router.get('/:roundId', getLeaderboard)
 
 export default router
