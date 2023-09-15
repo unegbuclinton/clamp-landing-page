f546e3ab093933e0d0813cf865a4697e5bb472c9
Clinton <unegbuclinton5@gmail.com>
2023-09-15 01:31:10 +0100

    upate responsiveness

--- src/components/molecules/WebPage/index.tsx
+++ src/components/molecules/WebPage/index.tsx
@@ -26,7 +26,6 @@ const WebPageSections = ({ isVisible }: { isVisible: boolean }) => {
         { id: 'section-six', start: 3361, end: 4100 },
       ]
 
-      console.log(scrollY)
       const active = sections.find((section) =>
         section.end
           ? scrollY >= section.start && scrollY < section.end
